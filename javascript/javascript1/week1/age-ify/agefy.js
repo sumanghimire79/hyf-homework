ac8891967f41c890281cca9eb6df656dafa6d445
sumanghimire79 <sumanghimire79@yahoo.com>
2021-10-27 00:32:42 +0200

    index.html file for agefy is committed

---
+++ javascript/javascript1/week1/age-ify/agefy.js
@@ -0,0 +1,5 @@
+/* suman ghimire javascript w1 homework agefy */
+var yearOfBirth = 1979;
+var yearFuture = 2090;
+var age = yearFuture - yearOfBirth; 
+console.log("You will be " +  age + " years old in " + yearFuture);
