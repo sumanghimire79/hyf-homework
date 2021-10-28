83501b02415394184489945b516c475d7e1463bd
sumanghimire79 <sumanghimire79@yahoo.com>
2021-10-28 11:02:18 +0200

    age-ify home work is revised according to mentor ! changed variable type var to const

--- javascript/javascript1/week1/age-ify/agefy.js
+++ javascript/javascript1/week1/age-ify/agefy.js
@@ -1,5 +1,5 @@
-/* suman ghimire javascript w1 homework agefy */
-var yearOfBirth = 1979;
-var yearFuture = 2090;
-var age = yearFuture - yearOfBirth; 
+/* suman ghimire javascript w1 homework Age-ify (A future age calculator) */
+const yearOfBirth = 1979;
+const yearFuture = 2090;
+const age = yearFuture - yearOfBirth; 
 console.log("You will be " +  age + " years old in " + yearFuture);
