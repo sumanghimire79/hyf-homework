b101449df79034857ec96bd2531761566ffde598
sumanghimire79 <sumanghimire79@yahoo.com>
2021-10-29 00:51:03 +0200

    condition set back to 'true' so that it logs the age of the dog in dog year

--- javascript/javascript1/week1/gooboy-oldboy/goodboy-oldboy.js
+++ javascript/javascript1/week1/gooboy-oldboy/goodboy-oldboy.js
@@ -4,7 +4,7 @@ const dogYearOfBirth = 2020;
 const dogYearFuture = 2045;
 const normalYear = dogYearFuture - dogYearOfBirth;
 const dogYear = 7 * normalYear; // dogyear in relation to human year. 1 human year equals 7 dog years !!
-const shouldShowResultInDogYears = false; 
+const shouldShowResultInDogYears = true; 
 
 if (shouldShowResultInDogYears){
     console.log("Your dog will be " + dogYear + " dog years old in " + dogYearFuture );
