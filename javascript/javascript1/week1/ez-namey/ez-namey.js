73fbb0c2beaba98e1bc1101f0777fc27db9af795
sumanghimire79 <sumanghimire79@yahoo.com>
2021-10-28 11:26:41 +0200

    reviewed but very minor changes

--- javascript/javascript1/week1/ez-namey/ez-namey.js
+++ javascript/javascript1/week1/ez-namey/ez-namey.js
@@ -1,9 +1,8 @@
-/* suman ghimire javascript w1 homework ez-namey */
+/* suman ghimire javascript w1 homework Ez Namey (Startup name generator) Optional */
 const firstWords = ["Easy", "Awesome", "Corporate", "Royal", "Welcome-Home" ];
 const secondWords = ["Start-up","Your Company", " New Market", "Dream-Team", "Best In Town"];
-let startupName;
 const randomFirstWords = firstWords[Math.floor(Math.random() * firstWords.length)];
 const randomSecondwords = secondWords[Math.floor(Math.random() * secondWords.length)];
-startupName = randomFirstWords + " " + randomSecondwords ;
+const startupName = randomFirstWords + " " + randomSecondwords ;
 const wordLength = startupName.length;
 console.log( " You can choose this new start-up company name " + '"'  + startupName + '"'  +". which has " + wordLength + " character !!");
