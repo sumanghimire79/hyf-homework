b608df7d9802b43ce2095bc973a49cfc0c788ed3
sumanghimire79 <sumanghimire79@yahoo.com>
2021-10-28 11:08:54 +0200

    1 human year = 7 dog year, method is implemented

--- javascript/javascript1/week1/gooboy-oldboy/goodboy-oldboy.js
+++ javascript/javascript1/week1/gooboy-oldboy/goodboy-oldboy.js
@@ -1,4 +1,4 @@
-/*suman ghimire javascript w1 homework goodboy-oldboy */
+/*suman ghimire javascript w1 homework Goodboy-Oldboy (A dog age calculator) */
 const yearOfBirth = 1979;
 const yearFuture = 2027;
 const age = yearFuture - yearOfBirth;
@@ -7,11 +7,12 @@ console.log("You will be " + age + " years old in " + yearFuture);
 
 const dogYearOfBirth = 2020;
 const dogYearFuture = 2045;
-const dogYear = dogYearFuture - dogYearOfBirth;
+const ownDogYear = dogYearFuture - dogYearOfBirth;
+const dogYear = 7 * ownDogYear; // dogyear in relation to human year. 1 human year equals 7 dog years !!
 const shouldShowResultInDogYears = true; 
 
 if (shouldShowResultInDogYears){
-    console.log("Your dog will be " + dogYear + " human years old in " + yearFuture);
+    console.log("Your dog will be " + dogYear + " dog years old in " + yearFuture + ". which is equal to " + ownDogYear + " human year .");
 }else {
-    console.log("Your dog will be " + age + " years old in " + yearFuture);
+    console.log("Your dog will be " + ownDogYear + " human years old in " + yearFuture);
 }
