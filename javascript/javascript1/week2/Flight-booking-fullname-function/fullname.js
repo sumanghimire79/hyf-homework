0f1125dd0a2be4ae033bd4ecc345b2a8dff595fa
sumanghimire79 <sumanghimire79@yahoo.com>
2021-11-02 23:03:30 +0100

    fullname.js and style.css has minar change in naming

--- javascript/javascript1/week2/Flight-booking-fullname-function/fullname.js
+++ javascript/javascript1/week2/Flight-booking-fullname-function/fullname.js
@@ -1,4 +1,13 @@
-function getFullname(fullname1, fullname2){
 
-    return 
+
+/* erercise Flight booking fullname function */
+
+function getFullname(fullname1, fullname2){
+let fullName = fullname1 + fullname2;
+    return fullName;
 }
+var myFullName = getFullname("Suman", " Ghimire");
+console.log (myFullName);
+var mySonsFullName = getFullname("Aayam", " Ghimire");
+console.log (mySonsFullName); 
+
