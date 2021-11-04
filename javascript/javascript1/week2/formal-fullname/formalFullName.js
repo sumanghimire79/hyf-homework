1a042ca341b0aa7b039e17c2cca8eb8edcd81be9
sumanghimire79 <sumanghimire79@yahoo.com>
2021-11-04 22:36:14 +0100

    gender argument is added in formalFullName.js

--- javascript/javascript1/week2/formal-fullname/formalFullName.js
+++ javascript/javascript1/week2/formal-fullname/formalFullName.js
@@ -4,18 +4,28 @@
 
 
 
-function getFullname(fullname1, fullname2, useFormalName ){
-    let fullName = fullname1 + fullname2 + useFormalName;
-       
-        if (useFormalName){
-            fullName = "Lord " + fullname1 + fullname2 ;
+function getFullname(fullname1, fullname2, useFormalName, gender ){
+    let fullName = fullname1 + fullname2 + useFormalName + gender;
+    
+        if (useFormalName && gender == "male"){
+            fullName = "Lord " + fullname1 + fullname2 + " " + gender;
 
-        }else {
-            fullName = fullname1 + fullname2 ;
-        }
+        } else if (useFormalName && gender == "female"){
+                fullName = "Miss " + fullname1 + fullname2 + " " + gender ;
+        }else if (!useFormalName && gender == "male"){
+            fullName =  fullname1 + fullname2 + " " + gender ;
+    } else {
+        fullName = fullname1 + fullname2 + " " + gender ;
+}
         return fullName;
     }
-    var myFullName = getFullname("Suman", " Ghimire", true);
+
+    // passing all possible four conditions using same name for example:
+    var myFullName = getFullname("Suman", " Ghimire", true , "male" );
     console.log (myFullName);
-    var mySonsFullName = getFullname("Aayam", " Ghimire", false);
+    var mySonsFullName = getFullname("Suman", " Ghimire", true, "female");
+    console.log (mySonsFullName);
+    var myFullName = getFullname("Suman", " Ghimire", false , "male" );
+    console.log (myFullName);
+    var mySonsFullName = getFullname("Suman", " Ghimire", false, "female");
     console.log (mySonsFullName);
