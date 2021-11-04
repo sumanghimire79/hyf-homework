9de2f3032445a721fc57a5223bbd6db67a2e735a
sumanghimire79 <sumanghimire79@yahoo.com>
2021-11-04 10:23:20 +0100

    queen added for class07

--- javascript/javascript1/week2/student-manager/studentManager.js
+++ javascript/javascript1/week2/student-manager/studentManager.js
@@ -5,17 +5,20 @@ const class07Students = [];
 function addStudentToClass(studentName) {
   // You write code here
   let admission;
-  if ( class07Students.length < 6){
+  if (class07Students.length < 6){
     class07Students.push(studentName);
     admission = class07Students.length+ " congratulation " + studentName+ " you are admitted";
     console.log(admission);
-  } else if (class07Students.includes(studentName)){
+    } else if (class07Students.includes(studentName)){
     admission = "Hi " + studentName +" you are already admitted"
     console.log(admission);
-  } else {
-    admission= " Sorry " + studentName + " we have already " +  class07Students.length + " students admitted. the class is full, Cannot add more students to class 07"
+    } else if (class07Students.length <= 6 && studentName === "Queen" ) {
+    admission= "Honourable " + studentName + " is successfully added to the class07." + " The class member now including queen is " + class07Students.length ;
     console.log(admission);  
-}
+    } else {
+     admission= " Sorry " + studentName + " we have already " +  class07Students.length + " students admitted. the class is full, Cannot add more students to class 07"
+    console.log(admission);  
+  }
 }
 
 function getNumberOfStudents() {
@@ -36,8 +39,7 @@ console.log(class07Students);
 addStudentToClass("Chunmai");
 console.log(class07Students);
 addStudentToClass("Asma");
-console.log(class07Students);
-addStudentToClass("Negar");
+
 console.log(class07Students);
 addStudentToClass("Annas");
 console.log(class07Students);
@@ -45,6 +47,13 @@ addStudentToClass("Suman");
 console.log(class07Students);
 addStudentToClass("Chunmai");
 console.log(class07Students);
-addStudentToClass("MR. Langsome");
+
+addStudentToClass("Queen");// array.length did not increase
 console.log(class07Students);
+
+addStudentToClass("MR. Langsome");
+
+console.log(class07Students);
+addStudentToClass("Queen"); // assigining queen again?
+
 getNumberOfStudents();
