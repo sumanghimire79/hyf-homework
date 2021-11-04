1cc677878185269c68a0c9df8f11f2684187530b
sumanghimire79 <sumanghimire79@yahoo.com>
2021-11-04 22:50:20 +0100

    Queen is pushed to the array beccause of a special condition reserved for Queen.The array.length is increased

--- javascript/javascript1/week2/student-manager/studentManager.js
+++ javascript/javascript1/week2/student-manager/studentManager.js
@@ -3,29 +3,30 @@
 
 const class07Students = [];
 function addStudentToClass(studentName) {
-  // You write code here
+
   let admission;
-  if (class07Students.length < 6){
+  if (class07Students.length < 6) {
     class07Students.push(studentName);
-    admission = class07Students.length+ " congratulation " + studentName+ " you are admitted";
+    admission = class07Students.length + " congratulation " + studentName + " you are admitted";
     console.log(admission);
-    } else if (class07Students.includes(studentName)){
-    admission = "Hi " + studentName +" you are already admitted"
+  } else if (class07Students.includes(studentName)) {
+    admission = "Hi " + studentName + " you are already admitted"
     console.log(admission);
-    } else if (class07Students.length <= 6 && studentName === "Queen" ) {
-    admission= "Honourable " + studentName + " is successfully added to the class07." + " The class member now including queen is " + class07Students.length ;
-    console.log(admission);  
-    } else {
-     admission= " Sorry " + studentName + " we have already " +  class07Students.length + " students admitted. the class is full, Cannot add more students to class 07"
-    console.log(admission);  
+  } else if (class07Students.length <= 6 && studentName === "Queen") {
+    class07Students.push(studentName);
+    admission = "Honourable " + studentName + " is successfully added to the class07." + " The class member now including queen is " + class07Students.length;
+    console.log(admission);
+  } else {
+    admission = " Sorry " + studentName + " we have already " + class07Students.length + " students admitted. the class is full, Cannot add more students to class 07"
+    console.log(admission);
   }
 }
 
 function getNumberOfStudents() {
   // You write code here
- let numberOfStudents = class07Students.length;
- console.log("The current number of Students is "+  numberOfStudents);
- return numberOfStudents;
+  let numberOfStudents = class07Students.length;
+  console.log("The current number of Students is " + numberOfStudents);
+  return numberOfStudents;
 
 }
 
@@ -48,12 +49,11 @@ console.log(class07Students);
 addStudentToClass("Chunmai");
 console.log(class07Students);
 
-addStudentToClass("Queen");// array.length did not increase
+addStudentToClass("Queen");// array.length increase as it is pushed
 console.log(class07Students);
 
 addStudentToClass("MR. Langsome");
-
 console.log(class07Students);
-addStudentToClass("Queen"); // assigining queen again?
 
+addStudentToClass("Queen"); // assigining queen again? No , does not allow duplicate entry
 getNumberOfStudents();
