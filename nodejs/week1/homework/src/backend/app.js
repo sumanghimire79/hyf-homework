d69b81781ff7a725adc7aeee60a08cc7bc53ac07
sumanghimire79 <sumanghimire79@yahoo.com>
2022-02-24 21:45:07 +0100

    reusable function and factoring

--- nodejs/week1/homework/src/backend/app.js
+++ nodejs/week1/homework/src/backend/app.js
@@ -1,102 +1,77 @@
 const express = require('express');
 const app = express();
+const cors = require('cors'); // to get api in localhost
+app.use(cors()); // to get api in localhost
 // import data here
 const meals = require('./data/meals');
 const reviews = require('./data/reviews');
 const reservations = require('./data/reservations');
 const colours = require('colors/safe');
+// const reusableCode = require('./data/reusableCode');
 // this is where you will be adding your routes
 app.get('/', async(request, response) => {
-    // response.send("asd");
     console.log(colours.rainbow('hello this is my first node message'));
-    response.send(colours.green('hello this is my first node message'));
+    response.send(`<div>
+    <span style="color: red">hello</span>
+    <span style="color: #1c37c6">this is</span>
+    <span style="color: #e91aaf">my first</span>
+    <span style="color: #e437a3">node</span>
+    <span style="color: #6e46da">message</span>
+  </div>`);
 });
+
+// reuseable code start
+const randomNumber = (n) => Math.round(Math.random() * n);
+const mealsWithReviews = meals.map((meal) => {
+    const reviewsForMeal = reviews.filter((review) => review.mealId === meal.id);
+    return {
+        ...meal,
+        reviews: reviewsForMeal,
+    };
+});
+// reuseable code ends
 
 // Respond with the json for all the meals.
 // For each meal, if there are reviews matching it's meal ID,
 // add these reviews to each meal in the form of an array.
 // For meals that do not have any reviews, the "reviews" property will be an empty array.
-
 app.get('/meals', async(request, response) => {
-    const mealsWithReviews = meals.map((meal) => {
-        const reviewsForMeal = reviews.filter(
-            (review) => review.mealId === meal.id,
-        );
-        return {
-            ...meal,
-            reviews: reviewsForMeal,
-        };
-    });
-    response.send(mealsWithReviews);
-
-    // response.json(mealsWithReviews);
+    response.status(200);
+    response.json(mealsWithReviews);
 });
 
 // Respond with the json for all the meals (including it's reviews) that are cheap
 app.get('/cheap-meals', async(request, response) => {
-    const allCheapMealWithReviews = meals
-        .filter((cheapmeal) => cheapmeal.price < 70)
-        .map((meal) => {
-            const reviewsForMeal = reviews.filter(
-                (review) => meal.id === review.mealId,
-            );
-            return {
-                ...meal,
-                review: reviewsForMeal,
-            };
-        });
-    // response.send(allCheapMealWithReviews);
-    response.json(allCheapMealWithReviews);
+    const cheapMealWithReviews = mealsWithReviews.filter(
+        (cheapmeal) => cheapmeal.price < 70,
+    );
+    response.json(cheapMealWithReviews);
 });
 // Respond with the json for all the meals (including it's reviews) that can fit lots of people
 app.get('/large-meals', async(request, response) => {
-    const allLargeMealWithReviews = meals
-        .filter((cheapmeal) => cheapmeal.maxNumberOfGuests > 5)
-        .map((meal) => {
-            const reviewsForMeal = reviews.filter(
-                (review) => meal.id === review.mealId,
-            );
-            return {
-                ...meal,
-                review: reviewsForMeal,
-            };
-        });
-    // response.send(allLargeMealWithReviews);
-    response.json(allLargeMealWithReviews);
+    const largeMealWithReviews = mealsWithReviews.filter(
+        (cheapmeal) => cheapmeal.maxNumberOfGuests > 5,
+    );
+    response.json(largeMealWithReviews);
 });
 
 // Respond with the json for a random meal (including it's reviews)
 app.get('/meal', async(request, response) => {
-    const allMeals = meals.map((meal) => meal);
-    const randomNumber = (n) => Math.floor(Math.random() * n);
-    const randomNumberCall = randomNumber(allMeals.length);
-    const randomFoods = allMeals[randomNumberCall];
-    const reviewsForMeal = reviews.filter(
-        (review) => randomFoods.id === review.mealId,
-    );
-    const allRandomMealsWithReviewsOBJ = {
-        ...randomFoods,
-        reviews: reviewsForMeal,
-    };
-    // response.send(allRandomMealsWithReviewsOBJ);
-    response.json(allRandomMealsWithReviewsOBJ);
+    const randomMealIndex = randomNumber(mealsWithReviews.length);
+    const randomMeal = mealsWithReviews[randomMealIndex];
+    response.json(randomMeal);
 });
 
 // Respond with the json for all reservations
 app.get('/reservations', async(request, response) => {
-    const allReservation = reservations.map((eachReservation) => eachReservation);
-    // response.send(allReservation);
-    response.json(allReservation);
+    response.json(reservations);
 });
 
 // Respond with the json for a random reservation
 app.get('/reservation', async(request, response) => {
-    const allReservation = reservations.map((eachReservation) => eachReservation);
-    const randN = (n) => Math.round(Math.random() * n);
-    const randomQuoteIndex = randN(allReservation.length);
-    const randomQuote = allReservation[randomQuoteIndex];
-    // response.send(randomQuote);
-    response.json(randomQuote);
+    const randomReservationIndex = randomNumber(reservations.length);
+    const randomReservation = reservations[randomReservationIndex];
+    response.json(randomReservation);
 });
 
 module.exports = app;
--- nodejs/week1/homework/src/backend/nodejs-w1_readMe.txt
+++ nodejs/week1/homework/src/backend/nodejs-w1_readMe.txt
@@ -1,12 +1,8 @@
 
-
-
-npm install (to install all modules/dependencies)
+npm install (to install all modules/dependencies)??
 npm install express (to install express module)
-npm install nodemon 
-npm install colors
-npm install node
-node
+[nodemon,colors,node]
+
 node/nodemon fileName.js
 npm run dev
 
@@ -15,3 +11,13 @@ node/nodemon fileName.js VS npm run dev ??
 
 npm init (to create package.json file which is basically the meta-data (in json format) of the project)
 
+json object how we convert to js obj while  require('./jsonFilename') ?
+does not show git status.
+ 
+us of colors in browser
+json formatting in browsr..
+how to access the local api ??
+
+mongoDB/noSql
+sql injection /data security  (tcl)
+sql grants(dcl)
