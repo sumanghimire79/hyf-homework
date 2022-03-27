f39d3916baf1fe65de06b343990b88d4c2169ff5
sumanghimire79 <sumanghimire79@yahoo.com>
2022-03-27 23:22:19 +0200

    refactored to separate methods for addition,substraction,multiply and division

--- nodejs/week3/homework/warmup/index.js
+++ nodejs/week3/homework/warmup/index.js
@@ -7,177 +7,205 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // get methods:
-app.get('/calculator', (req, res) => {
-    const firstParam = req.query.firstParam;
-    const secondParam = req.query.secondParam;
-    const firstParamErrorMsg = ' first param is not a number';
-    const secondParamErrorMsg = ' the second param is not a number';
-    let answer;
+app.get('/calculator/addition', (req, res) => {
+  const firstParam = req.query.firstParam;
+  const secondParam = req.query.secondParam;
+  const firstParamErrorMsg = ' first param is not a number';
+  const secondParamErrorMsg = ' the second param is not a number';
+  let answer;
 
-    if (isNaN(firstParam)) {
-        return res.send(firstParamErrorMsg);
+  if (isNaN(firstParam)) {
+    return res.send(firstParamErrorMsg);
+  }
+  if (typeof secondParam === 'object') {
+    answer =
+      secondParam.reduce((one, another) => Number(one) + Number(another)) +
+      Number(firstParam);
+
+    if (isNaN(answer)) {
+      return res.send(secondParamErrorMsg);
     }
+    return res.json(answer);
+  }
+  if (isNaN(secondParam)) {
+    return res.send(secondParamErrorMsg);
+  }
+  answer = Number(firstParam) + Number(secondParam);
 
-    if ('addition' in req.query) {
-        if (typeof secondParam === 'object') {
-            answer =
-                secondParam.reduce((one, another) => Number(one) + Number(another)) +
-                Number(firstParam);
+  res.json(answer);
+});
 
-            if (isNaN(answer)) {
-                return res.send(secondParamErrorMsg);
-            }
-            return res.json(answer);
-        }
-        if (isNaN(secondParam)) {
-            return res.send(secondParamErrorMsg);
-        }
-        answer = Number(firstParam) + Number(secondParam);
+app.get('/calculator/substraction', (req, res) => {
+  const firstParam = req.query.firstParam;
+  const secondParam = req.query.secondParam;
+  const firstParamErrorMsg = ' first param is not a number';
+  const secondParamErrorMsg = ' the second param is not a number';
+  let answer;
 
-        res.json(answer);
+  if (isNaN(firstParam)) {
+    return res.send(firstParamErrorMsg);
+  }
+
+  if (typeof secondParam === 'object') {
+    answer = secondParam.reduce(
+      (one, another) => Number(one) + Number(another),
+    );
+
+    if (isNaN(answer)) {
+      return res.send(secondParamErrorMsg);
     }
+    return res.json(Number(firstParam) - answer);
+  }
+  if (isNaN(secondParam)) {
+    return res.send(secondParamErrorMsg);
+  }
+  answer = Number(firstParam) - Number(secondParam);
 
-    if ('substraction' in req.query) {
-        if (typeof secondParam === 'object') {
-            answer = secondParam.reduce(
-                (one, another) => Number(one) + Number(another),
-            );
+  res.json(answer);
+});
 
-            if (isNaN(answer)) {
-                return res.send(secondParamErrorMsg);
-            }
-            return res.json(Number(firstParam) - answer);
-        }
-        if (isNaN(secondParam)) {
-            return res.send(secondParamErrorMsg);
-        }
-        answer = Number(firstParam) - Number(secondParam);
+app.get('/calculator/multiplication', (req, res) => {
+  const firstParam = req.query.firstParam;
+  const secondParam = req.query.secondParam;
+  const firstParamErrorMsg = ' first param is not a number';
+  const secondParamErrorMsg = ' the second param is not a number';
+  let answer;
 
-        res.json(answer);
+  if (isNaN(firstParam)) {
+    return res.send(firstParamErrorMsg);
+  }
+
+  if (typeof secondParam === 'object') {
+    answer =
+      secondParam.reduce((one, another) => Number(one) * Number(another)) *
+      Number(firstParam);
+
+    if (isNaN(answer)) {
+      return res.send(secondParamErrorMsg);
     }
+    return res.json(answer);
+  }
+  if (isNaN(secondParam)) {
+    return res.send(secondParamErrorMsg);
+  }
+  answer = Number(firstParam) * Number(secondParam);
 
-    if ('multiplication' in req.query) {
-        if (typeof secondParam === 'object') {
-            answer =
-                secondParam.reduce((one, another) => Number(one) * Number(another)) *
-                Number(firstParam);
+  res.json(answer);
+});
 
-            if (isNaN(answer)) {
-                return res.send(secondParamErrorMsg);
-            }
-            return res.json(answer);
-        }
-        if (isNaN(secondParam)) {
-            return res.send(secondParamErrorMsg);
-        }
-        answer = Number(firstParam) * Number(secondParam);
+app.get('/calculator/division', (req, res) => {
+  const firstParam = req.query.firstParam;
+  const secondParam = req.query.secondParam;
+  const firstParamErrorMsg = ' first param is not a number';
+  const secondParamErrorMsg = ' the second param is not a number';
+  let answer;
 
-        res.json(answer);
+  if (isNaN(firstParam)) {
+    return res.send(firstParamErrorMsg);
+  }
+
+  if (typeof secondParam === 'object') {
+    answer = secondParam.reduce(
+      (one, another) => Number(one) * Number(another),
+    );
+    if (isNaN(answer)) {
+      return res.send(secondParamErrorMsg);
     }
-    if ('division' in req.query) {
-        if (typeof secondParam === 'object') {
-            answer = secondParam.reduce(
-                (one, another) => Number(one) * Number(another),
-            );
-            if (isNaN(answer)) {
-                return res.send(secondParamErrorMsg);
-            }
-            return res.json(Number(firstParam) / answer);
-        }
-        if (isNaN(secondParam)) {
-            return res.send(secondParamErrorMsg);
-        }
-        answer = Number(firstParam) / Number(secondParam);
+    return res.json(Number(firstParam) / answer);
+  }
+  if (isNaN(secondParam)) {
+    return res.send(secondParamErrorMsg);
+  }
+  answer = Number(firstParam) / Number(secondParam);
 
-        res.json(answer);
-    }
+  res.json(answer);
 });
 
 //  post method
 app.post('/calculator', (req, res) => {
-    const firstParam = req.body.firstParam;
-    const secondParam = req.body.secondParam;
-    const firstParamErrorMsg = ' first param is not a number';
-    const secondParamErrorMsg = ' the second param is not a number';
-    let answer;
+  const firstParam = req.body.firstParam;
+  const secondParam = req.body.secondParam;
+  const firstParamErrorMsg = ' first param is not a number';
+  const secondParamErrorMsg = ' the second param is not a number';
+  let answer;
 
-    if (isNaN(firstParam)) {
-        return res.send(firstParamErrorMsg);
+  if (isNaN(firstParam)) {
+    return res.send(firstParamErrorMsg);
+  }
+
+  if ('addition' in req.body) {
+    if (typeof secondParam === 'object') {
+      answer =
+        secondParam.reduce((one, another) => Number(one) + Number(another)) +
+        Number(firstParam);
+
+      if (isNaN(answer)) {
+        return res.send(secondParamErrorMsg);
+      }
+      return res.json(answer);
     }
-
-    if ('addition' in req.body) {
-        if (typeof secondParam === 'object') {
-            answer =
-                secondParam.reduce((one, another) => Number(one) + Number(another)) +
-                Number(firstParam);
-
-            if (isNaN(answer)) {
-                return res.send(secondParamErrorMsg);
-            }
-            return res.json(answer);
-        }
-        if (isNaN(secondParam)) {
-            return res.send(secondParamErrorMsg);
-        }
-        answer = Number(firstParam) + Number(secondParam);
-
-        res.json(answer);
+    if (isNaN(secondParam)) {
+      return res.send(secondParamErrorMsg);
     }
+    answer = Number(firstParam) + Number(secondParam);
 
-    if ('substraction' in req.body) {
-        if (typeof secondParam === 'object') {
-            answer = secondParam.reduce(
-                (one, another) => Number(one) + Number(another),
-            );
+    res.json(answer);
+  }
 
-            if (isNaN(answer)) {
-                return res.send(secondParamErrorMsg);
-            }
-            return res.json(Number(firstParam) - answer);
-        }
-        if (isNaN(secondParam)) {
-            return res.send(secondParamErrorMsg);
-        }
-        answer = Number(firstParam) - Number(secondParam);
+  if ('substraction' in req.body) {
+    if (typeof secondParam === 'object') {
+      answer = secondParam.reduce(
+        (one, another) => Number(one) + Number(another),
+      );
 
-        res.json(answer);
+      if (isNaN(answer)) {
+        return res.send(secondParamErrorMsg);
+      }
+      return res.json(Number(firstParam) - answer);
     }
-
-    if ('multiplication' in req.body) {
-        if (typeof secondParam === 'object') {
-            answer =
-                secondParam.reduce((one, another) => Number(one) * Number(another)) *
-                Number(firstParam);
-
-            if (isNaN(answer)) {
-                return res.send(secondParamErrorMsg);
-            }
-            return res.json(answer);
-        }
-        if (isNaN(secondParam)) {
-            return res.send(secondParamErrorMsg);
-        }
-        answer = Number(firstParam) * Number(secondParam);
-
-        res.json(answer);
+    if (isNaN(secondParam)) {
+      return res.send(secondParamErrorMsg);
     }
-    if ('division' in req.body) {
-        if (typeof secondParam === 'object') {
-            answer = secondParam.reduce(
-                (one, another) => Number(one) * Number(another),
-            );
-            if (isNaN(answer)) {
-                return res.send(secondParamErrorMsg);
-            }
-            return res.json(Number(firstParam) / answer);
-        }
-        if (isNaN(secondParam)) {
-            return res.send(secondParamErrorMsg);
-        }
-        answer = Number(firstParam) / Number(secondParam);
+    answer = Number(firstParam) - Number(secondParam);
 
-        res.json(answer);
+    res.json(answer);
+  }
+
+  if ('multiplication' in req.body) {
+    if (typeof secondParam === 'object') {
+      answer =
+        secondParam.reduce((one, another) => Number(one) * Number(another)) *
+        Number(firstParam);
+
+      if (isNaN(answer)) {
+        return res.send(secondParamErrorMsg);
+      }
+      return res.json(answer);
     }
+    if (isNaN(secondParam)) {
+      return res.send(secondParamErrorMsg);
+    }
+    answer = Number(firstParam) * Number(secondParam);
+
+    res.json(answer);
+  }
+  if ('division' in req.body) {
+    if (typeof secondParam === 'object') {
+      answer = secondParam.reduce(
+        (one, another) => Number(one) * Number(another),
+      );
+      if (isNaN(answer)) {
+        return res.send(secondParamErrorMsg);
+      }
+      return res.json(Number(firstParam) / answer);
+    }
+    if (isNaN(secondParam)) {
+      return res.send(secondParamErrorMsg);
+    }
+    answer = Number(firstParam) / Number(secondParam);
+
+    res.json(answer);
+  }
 });
 
 app.listen(3000, () => console.log(`Calculator:listening on port 3000`));
