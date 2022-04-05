34b1159c5b97d885a8616428ece7c5adbe28b1a9
sumanghimire79 <sumanghimire79@yahoo.com>
2022-04-05 09:25:43 +0200

    react1week3-shifts

---
+++ react/react1/week3/react1week3-shifts/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import './components/shift.css';
+import { Counter } from './components/Counter';
+import { InputShift } from './components/InputShift';
+
+function App() {
+  return (
+    <div className="App">
+      <Counter />
+
+      <InputShift />
+    </div>
+  );
+}
+
+export default App;
---
+++ react/react1/week3/react1week3-shifts/src/components/DataDefaultShifts.js
@@ -0,0 +1,4 @@
+export const dataDfaultShift = [
+  { id: 1, name: 'suman', startTime: '20:20', endTime: '20:40' },
+  { id: 2, name: 'Amrit', startTime: '21:20', endTime: '21:40' },
+];
