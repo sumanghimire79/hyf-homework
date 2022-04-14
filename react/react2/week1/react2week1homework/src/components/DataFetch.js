e5ec57930f51a8ca1c9e9253b0fbfb9aa7c895fb
sumanghimire79 <sumanghimire79@yahoo.com>
2022-04-14 11:47:23 +0200

    .then changed to async await

--- react/react2/week1/react2week1homework/src/components/DataFetch.js
+++ react/react2/week1/react2week1homework/src/components/DataFetch.js
@@ -5,57 +5,39 @@ export function DataFetch() {
   const UseContext = useContext(SearchContext);
   const query = UseContext.searchInput;
 
-  const [isLoading, SetIsLoading] = useState(true);
+  const [isLoading, SetIsLoading] = useState(false);
   const [userFetched, setUserFetched] = useState([]);
-  const [errorMessage, setErrorMessage] = useState('');
+  const [errorHandling, setErrorHandling] = useState();
 
   const Api = `https://api.github.com/search/users?q=${query}`;
   useEffect(() => {
+    if (!Api) {
+      setUserFetched([]);
+      // return;
+    }
     SetIsLoading(true);
-
-    fetch(Api)
-      .then((res) => res.json())
-      .then((data) => {
-        console.log('data??', data.message);
-
-        if (query.length === 0) {
-          setUserFetched([]);
-        } else {
-          if (data.length === 0) {
-            setUserFetched([]);
-          } else {
-            const fetchMap = data.items.map((data) => <li>{data.login}</li>);
-
-            setUserFetched(fetchMap);
-          }
-        }
+    (async () => {
+      try {
+        const api = await fetch(Api);
+        const jsonData = await api.json();
+        const fetchMap = jsonData.items.map((data) => data.login);
+        setUserFetched(fetchMap);
+      } catch (err) {
+        // throw err;
+        setErrorHandling(err);
+        console.log(errorHandling);
+      } finally {
         SetIsLoading(false);
-      })
-      .catch((error) => {
-        console.log(` error is ${error}`);
+      }
+    })();
+  }, [Api, query]);
 
-        setErrorMessage(` ${error}`);
-      });
-  }, [Api, query.length]);
-
-  console.log(userFetched);
-  console.log(query);
-
+  const item = userFetched.map((user, index) => <li key={index}>{user}</li>);
   return (
     <div>
-      {/* {isLoading ? (
-        'Loading....'
-      ) : (
-        <div> {userFetched.length === 0 ? ' no items' : userFetched}</div>
-      )} */}
-
       {isLoading && <p> Loading....</p>}
-
-      {errorMessage ? (
-        errorMessage
-      ) : (
-        <div> {userFetched.length === 0 ? ' no items' : userFetched}</div>
-      )}
+      {/* {errorHandling} */}
+      <div>{userFetched.length === 0 ? ' no items' : item}</div>
     </div>
   );
 }
